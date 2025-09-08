4651b1ad8cd2509849cf0ff22b083f0fa0dfd034
Ayare Punit Sunil <punit.235857101@vcet.edu.in>
2025-09-09 00:15:05 +0530

    Update App.js

--- frontend/career_dev/src/App.js
+++ frontend/career_dev/src/App.js
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import ResumeForm from "./ResumeForm";
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [greeting, setGreeting] = useState("");
-
-  // Call FastAPI backend when page loads
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
-  // Button click → call backend
-  const fetchGreeting = () => {
-    fetch("http://127.0.0.1:8000/api/greet/Punit")
-      .then((res) => res.json())
-      .then((data) => setGreeting(data.greeting));
-  };
-
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>{message}</h1>
-      <button onClick={fetchGreeting}>Get Greeting</button>
-      <h2>{greeting}</h2>
+      <h1>AI Resume Analyzer</h1>
+      <ResumeForm />
     </div>
   );
 }
